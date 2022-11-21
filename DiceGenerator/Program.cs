aad5c91483012d6a0274deeb7ee54e29827406d9
Namitha Augustine <48145657+NamithaAugustine@users.noreply.github.com>
2022-11-21 09:47:03 +0530

    Add files via upload

---
+++ DiceGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int i,d;
+Console.Write("Press 1 to roll dice");
+i = Convert.ToInt32(Console.ReadLine());
+while (i==1)
+{
+    Random rand = new Random();
+    int number = rand.Next(1, 7);
+    Console.WriteLine(number);
+    Console.Write("Press 1 to roll dice");
+    i = Convert.ToInt32(Console.ReadLine());
+}
+
